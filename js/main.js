7caa3a8608a7030f00e5cc8c122804ff89b73b33
Andrii Honchar <goncharas1988@gmail.com>
2022-11-06 23:04:11 +0200

    add burger

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@
         }
     }
 }());
+
+// burger handler
+(function (){
+    const burgerItem = document.querySelector('.burger')
+    const menu = document.querySelector('.header__nav')
+    const menuCloseItem = document.querySelector(".headed__nav-close");
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__nav_active');
+    });
+}());
